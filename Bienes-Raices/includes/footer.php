fd18a194fa6fbb940d28dbef19ce04fd424ce888
ThReynosoDxD <gaelomreynoso@gmail.com>
2024-10-29 07:48:45 -0700

    Add files via upload

---
+++ Bienes-Raices/includes/footer.php
@@ -0,0 +1,16 @@
+<section>
+        <footer>
+            <div>
+            <nav>
+            <a href = "index.php">Inicio</a>
+                <a href = "Nosotros.php">Nosotros</a>
+                <a href = "">Anuncios</a>
+                <a href = "Blog.php">Blog</a>
+                <a href = "Contactanos.php">Contactanos</a>
+            </nav>
+            </div>
+            <div>
+                <p>TODOS LOS DERECHOS RESERVADOS BY I</p>
+            </div>
+        </footer>
+    </section>
